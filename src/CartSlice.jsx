5ffd145ebc4acc5a9a408c375d27dd3f46ec8788
Michael <mltor8@gmail.com>
2026-05-15 12:45:44 -0400

    functionality update

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      state.items.push(action.payload);
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (!existingItem) {
+        state.items.push({ name, image, cost, quantity: 1 });
+        // existingItem.quantity++;
+      } 
+      // else {
+      //   state.items.push({ name, image, cost, quantity: 1 });
+      // }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+      }
     },
   },
 });
